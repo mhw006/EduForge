dd6145e0ffa8023b0b71a1ba62a08c1d2e40af76
Israel Trejo <isrtrejo07@gmail.com>
2026-04-24 22:24:56 -0700

    frontend page shows up

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">EduForge</h1>
+    </main>
+  )
+}
